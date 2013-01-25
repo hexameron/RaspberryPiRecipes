735a4c8344baa6dc67bc6396108dd415fd505660
Rob Bishop <quantum.rob@gmail.com>
2013-01-25 12:23:52 +0000

    Update InteractiveJellyBaby.py

--- InteractiveJellyBaby.py
+++ InteractiveJellyBaby.py
@@ -1,13 +1,15 @@
+# Import the Python libraries that we need for this program
 import time
 import os
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.IN)
-GPIO.setup(24, GPIO.IN)
-GPIO.setup(25, GPIO.IN)
+# Set-up the General Purpose Input-Ouput (GPIO) pins
+GPIO.cleanup() # Start from scratch
+GPIO.setmode(GPIO.BCM) # Set GPIO mode for the Pi
+GPIO.setup(2, GPIO.IN) # Set Pin 2 on the GPIO header to be an input
 
-while True:
-        if GPIO.input(4):
-                os.system('mpg321 1748.mp3 &')
-        time.sleep(1);
+# This loop runs forever and plays the mp3 file when the two wires are touching
+while True: # Loop forever
+        if GPIO.input(2) == False: # Check to see if GPIO pin 2 is connected to the ground pin
+                os.system('mpg321 1748.mp3 &') # If it is then play the mp3 file
+        time.sleep(1); # Wait for a second before repeating the loop
